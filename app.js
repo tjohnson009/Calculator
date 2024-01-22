2bc4a58babd93ab58e707ad14f1b5064772dc1a6
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-01-21 22:08:08 -0500

    Set up OOP structure with Calculator class and Button class

--- app.js
+++ app.js
@@ -1,3 +1,65 @@
-function styleButtons() {
-    
+let DOM = {
+
+}; 
+
+class Calculator {
+    constructor(element) {
+        this.element = element; 
+        this.input = '';
+        this.output = ''; 
+        this.a = ''; 
+        this.operator = ''; 
+        this.b = ''; 
+    }
+
+    add = (a, b) => {
+        return a + b; 
+    }
+
+    subtract = (a, b) => {
+        return a - b; 
+    };
+
+    multiply = (a, b) => {
+        return a * b; 
+    }
+
+    divide = (a, b) => {
+        if (b !== 0) {
+            return a / b;  
+        } else {
+            return `Error!`; 
+        }
+    }
+
+    updateInput = (newInput) => {
+        return this.input = newInput; 
+    }
+
+    updateOutput = () => {
+
+    }
+}
+
+class Button {
+    constructor(element, value) {
+        this.element = element; 
+        this.value = value; 
+    }
+
+    addToInput = () => {
+        
+    }
+
+    addClickListener(callback) {
+        this.element.addEventListener('click', callback)
+    }
+
+    addKeypressListener(callback, keyCode) {
+        this.element.addEventListener('keypress', () => {
+            if (e.target.keyCode === keyCode) {
+                callback(); 
+            }
+        })
+    }
 }
